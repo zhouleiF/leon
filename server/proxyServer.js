6244a2cf9b4cfd03d4f28b8deea0a0cf8c2a8bf3
周磊 <lei.zhou@dianrong.com>
2017-05-09 16:04:38 +0800

    update

--- server/proxyServer.js
+++ server/proxyServer.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 var proxy = require('express-http-proxy')
 
-app.use('/jsconsole', proxy('localhost:8000'))
+app.use('/jsconsole', proxy('http://localhost:8000/'))
 app.use('/weinre', proxy('localhost:8001'))
 app.listen(80, function() {
     console.log('Example app listening on port 80!')
